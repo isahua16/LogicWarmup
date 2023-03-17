816aadd4dfaca28a7c5382d60c7db63290332e06
isahua16 <isaelhuard@gmail.com>
2023-03-16 20:49:19 -0600

    Played with large logic conditional statements

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 let tickets_sold = 5000;
 let venue_capacity = 6000
 let performer = "Bon Jovi";
+let special_perf_one = `Alex Bymoen`;
+let special_perf_two = `Elvis Presley`;
+let special_perf_three = `1972 Miami Dolphins`;
+let special_perf_four = `Elton John`;
+let special_perf_five = `Queen`;
 let is_sold_out = false;
 let is_repeat_show = true;
 let percent_sold = tickets_sold / venue_capacity;
@@ -37,3 +42,8 @@ if (((percent_sold >= 0.9 || venue_capacity >= 6000) && (percent_sold >= 0.7 || 
     console.log("Normal Case");
 }
 
+if ((special_perf_one === `Alex Bymoen` || special_perf_one === `Shrek` || special_perf_one === `1972 Miami Dolphins`) || (special_perf_two === `Alex Bymoen` || special_perf_two === `Shrek` || special_perf_two === `1972 Miami Dolphins`) || (special_perf_three === `Alex Bymoen` || special_perf_three === `Shrek` || special_perf_three === `1972 Miami Dolphins`) || (special_perf_four === `Alex Bymoen` || special_perf_four === `Shrek` || special_perf_four === `1972 Miami Dolphins`) || (special_perf_five === `Alex Bymoen` || special_perf_five === `Shrek` || special_perf_five === `1972 Miami Dolphins`) )  {
+    console.log(`Lucky you!`);
+} else {
+    console.log(`Too Bad!`);
+}
